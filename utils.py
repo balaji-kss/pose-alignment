49711e9b193a46f1580b75d1d0272f3132549cfd
Balaji <kss.balaji1@gmail.com>
2024-01-15 16:37:56 -0800

    rendering WIP

--- utils.py
+++ utils.py
@@ -2,8 +2,19 @@ import mmcv
 import numpy as np
 import cv2
 import pickle
+import render 
 
-def number_frames(input_video_path, output_video_path):
+def number_frames():
+
+    vname = 'baseline'
+    act_name = 'Lower_Galley_Carrier'
+    root_dir = '/home/tumeke-balaji/Documents/results/delta/joints/' + act_name + '/'
+    input_video_path =  root_dir + vname + '/' + vname + '.mov'
+    ext = input_video_path.rsplit('.', 1)[1]
+    output_video_path = input_video_path.rsplit('.', 1)[0] + '_n.' + ext
+    print('input_video_path ', input_video_path)
+    print('output_video_path ', output_video_path)
+
     # Load video using mmcv
     video = mmcv.VideoReader(input_video_path)
     
@@ -133,6 +144,91 @@ def video_align():
             cv2.imshow('Candidate ', frame2)
             cv2.waitKey(-1)
         
+def render_entire_video():
+    
+    name = 'Stowing_carrier'
+    filename = 'baseline'
+    video_path = "/home/tumeke-balaji/Documents/results/delta/joints/" + name + "/" + filename + "/" + filename + ".mov"
+    output_video_path = "/home/tumeke-balaji/Documents/results/delta/joints/" + name + "/" + filename + "/render_" + filename + ".mov"
+    pose_path_3d = "/home/tumeke-balaji/Documents/results/delta/joints/" + name + "/" + filename + "/pose_3d.p"
+    pose_path_2d = "/home/tumeke-balaji/Documents/results/delta/joints/" + name + "/" + filename + "/pose_2d.p"
+    
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    
+    with open(pose_path_2d, 'rb') as f:
+        poses_2d = pickle.load(f)
+            
+    with open(pose_path_3d, 'rb') as f:
+        poses_3d = pickle.load(f)
+
+    video = mmcv.VideoReader(video_path)
+    num_frames = len(video)
+
+    video_writer = cv2.VideoWriter(output_video_path,
+                            fourcc, video.fps, (video.width, video.height))
+    
+    print(len(poses_3d))
+    for i in range(num_frames):
+
+        frame = video[i]
+        if len(poses_3d[i]):
+            joints_3d = poses_3d[i][0]['keypoints_3d']
+            joints_2d = poses_3d[i][0]['keypoints']
+
+            face_2d = poses_2d[i][0]['keypoints_2d'][[0, 1, 2, 3, 4, 17]]                   
+            joints_2d = np.concatenate((joints_2d, face_2d), axis = 0)
+
+            frame = render.drawSkeleton(frame, joints_2d, [0]*9, base=True, thresh=0.35)
+
+        video_writer.write(frame)
+        # cv2.imshow('frame ', frame)
+        # cv2.waitKey(-1)
+
+    video_writer.release()
+
+def merge_videos():
+
+    name = 'Stowing_carrier'
+    bvideo_path = "/home/tumeke-balaji/Documents/results/delta/joints/" + name + "/baseline/render_baseline.mov"
+    cvideo_path = "/home/tumeke-balaji/Documents/results/delta/joints/" + name + "/candidate/render_candidate.mov"
+    dev_video = '/home/tumeke-balaji/Documents/results/delta/joints/' + name + '/' + name + '.mov'
+    output_video_path = '/home/tumeke-balaji/Documents/results/delta/joints/' + name + '/' + name + '_all.mov'
+
+    bvideo = mmcv.VideoReader(bvideo_path)
+    cvideo = mmcv.VideoReader(cvideo_path)
+    dvideo = mmcv.VideoReader(dev_video)
+
+    v1_seg = [[60, 137], [137, 140], [140, 172], [172, 175], [175, 260], [260, 280], [280, 385]] # baseline
+    v2_seg = [[100, 180], [180, 225], [225, 260], [260, 260], [260, 320], [320, 360], [360, 510]] # candidate
+
+    gidx = 0
+
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    video_writer = cv2.VideoWriter(output_video_path,
+                                fourcc, cvideo.fps, (1080, 960))
+    
+    for i in range(len(v1_seg)):
+
+        bseg, cseg = normalize_segment(v1_seg[i], v2_seg[i])
+
+        for b, c in zip(bseg, cseg):
+
+            if i % 2 == 0:
+                out = dvideo[gidx]
+                gidx += 1
+                cv2.imshow('out ', out)
+            else:
+                bframe = bvideo[b]
+                cframe = cvideo[c]
+                out = np.hstack((bframe, cframe))
+                out = cv2.resize(out, None, fx = 0.5, fy = 0.5)
+                cv2.imshow('out ', out)
+            
+            cv2.waitKey(-1)
+            video_writer.write(out)
+        
+    video_writer.release()
+
 if __name__ == "__main__":  
 
     connections = [
@@ -158,15 +254,11 @@ if __name__ == "__main__":
 
 
     # Index frames in a video and save it
-    vname = 'baseline'
-    act_name = 'Lower_Galley_Carrier'
-    root_dir = '/home/tumeke-balaji/Documents/results/delta/joints/' + act_name + '/'
-    input_video_path =  root_dir + vname + '/' + vname + '.mov'
-    ext = input_video_path.rsplit('.', 1)[1]
-    output_video_path = input_video_path.rsplit('.', 1)[0] + '_n.' + ext
-    print('input_video_path ', input_video_path)
-    print('output_video_path ', output_video_path)
-    number_frames(input_video_path, output_video_path)
+    # number_frames()
 
     # check start and end of action for video alignment
     # video_align()
+
+    # render_entire_video()
+
+    merge_videos()
